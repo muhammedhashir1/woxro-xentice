19719e96a9c3d42660b60348c6fd7a4dedbd1876
Muhammed Hashir <hashiraachi818@gmail.com>
2022-09-30 12:09:49 +0530

    iconsbar works started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar/NavBar';
+import Iconsbar from './Components/Iconsbar/Iconsbar';
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Iconsbar/>
      </div> 
   );
 }
