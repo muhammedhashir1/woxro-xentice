cd1186105b9ee947e3bb4e0cd36ddb3a9b6e84ea
Muhammed Hashir <hashiraachi818@gmail.com>
2022-09-30 19:25:34 +0530

    banner completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar/NavBar';
 import Iconsbar from './Components/Iconsbar/Iconsbar';
+import Banner from './Components/Banner/Banner';
 
 function App() {
   return (
     <div>
       <NavBar/>
       <Iconsbar/>
+      <Banner/>
      </div> 
   );
 }
